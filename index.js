79255d58219bab76f4cfdc14ed82d6aedeacc342
Michael <michael.mhanna86@gmail.com>
2020-01-10 00:02:24 -0500

    modifications

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const pdf = require("html-pdf")
 const open = require("open")
 const generateHTML = require("./generateHTML")
 
-
-const data = {}
 const questions = [{
     type: "input",
     name: "username",
-    message: "Enter your GitHub username: ",
+    message: "please Enter your GitHub username: ",
 },
 {
     type: "list",
     name: "color",
-    message: "What is your favorite color?",
+    message: "please choose a color?",
     choices: ["green", "blue", "pink", "red"],
 }];
 
